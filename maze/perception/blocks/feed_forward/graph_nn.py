RL-1096: fixed gradient propagation bug by adding the option to select node and edge embeddings explicitly
(Issue RL-1096 - Vanilla Message Passing GNN)

maze/perception/blocks/feed_forward/graph_nn.py
@@ -98,6 +98,8 @@ class GNNBlock(ShapeNormalizationBlock):
     :param edge2node_aggr: If True edge to node message passing is applied.
     :param node2edge_aggr: If True node to edge message passing is applied.
     :param edge2edge_aggr: If True edge to edge message passing is applied.
+    :param with_node_embedding: If True the node embedding is computed.
+    :param with_edge_embedding: If True the edge embedding is computed.
     """
 
     def __init__(self,
@@ -112,7 +114,9 @@ class GNNBlock(ShapeNormalizationBlock):
                  node2node_aggr: bool,
                  edge2node_aggr: bool,
                  node2edge_aggr: bool,
-                 edge2edge_aggr: bool
+                 edge2edge_aggr: bool,
+                 with_node_embedding: bool,
+                 with_edge_embedding: bool
                  ):
         super().__init__(in_keys=in_keys, out_keys=out_keys, in_shapes=in_shapes,
                          in_num_dims=[3, 3], out_num_dims=[3, 3])
@@ -131,6 +135,9 @@ class GNNBlock(ShapeNormalizationBlock):
         self.node2edge_aggr = node2edge_aggr
         self.edge2edge_aggr = edge2edge_aggr
 
+        self.with_node_embedding = with_node_embedding
+        self.with_edge_embedding = with_edge_embedding
+
         self.hidden_units = hidden_units
         self.n_layers = len(self.hidden_units)
         self.non_lin = Factory(base_type=nn.Module).type_from_name(non_lin)
@@ -140,26 +147,30 @@ class GNNBlock(ShapeNormalizationBlock):
         self.edge_list = np.sort(self.edge_list, axis=0)
 
         # node layers
-        self.node_layers = nn.ModuleList()
-        for i in range(self.n_layers):
+        if with_node_embedding or node2edge_aggr or edge2node_aggr:
+            self.node_layers = nn.ModuleList()
+            for i in range(self.n_layers):
 
-            if i == 0:
-                in_dim = self.num_node_features
-            else:
-                in_dim = 2 * self.hidden_units[i - 1] if self.edge2node_aggr else self.hidden_units[i - 1]
+                if i == 0:
+                    in_dim = self.num_node_features
+                else:
+                    in_dim = 2 * self.hidden_units[i - 1] if self.edge2node_aggr else self.hidden_units[i - 1]
 
-            self.node_layers.append(self._make_sub_layer(in_dim=in_dim, out_dim=self.hidden_units[i]))
+                if with_node_embedding or (i + 1) < self.n_layers:
+                    self.node_layers.append(self._make_sub_layer(in_dim=in_dim, out_dim=self.hidden_units[i]))
 
         # edge layers
-        self.edge_layers = nn.ModuleList()
-        for i in range(self.n_layers):
+        if with_edge_embedding or edge2node_aggr or node2edge_aggr:
+            self.edge_layers = nn.ModuleList()
+            for i in range(self.n_layers):
 
-            if i == 0:
-                in_dim = self.num_edge_features
-            else:
-                in_dim = 2 * self.hidden_units[i - 1] if self.node2edge_aggr else self.hidden_units[i - 1]
+                if i == 0:
+                    in_dim = self.num_edge_features
+                else:
+                    in_dim = 2 * self.hidden_units[i - 1] if self.node2edge_aggr else self.hidden_units[i - 1]
 
-            self.edge_layers.append(self._make_sub_layer(in_dim=in_dim, out_dim=self.hidden_units[i]))
+                if with_edge_embedding or (i + 1) < self.n_layers:
+                    self.edge_layers.append(self._make_sub_layer(in_dim=in_dim, out_dim=self.hidden_units[i]))
 
         # prepare aggregation layers
         self.aggregate = aggregate
@@ -187,7 +198,17 @@ class GNNBlock(ShapeNormalizationBlock):
         assert node_embedding.ndim == self.out_num_dims[0]
         assert edge_embedding.ndim == self.out_num_dims[1]
 
-        return {self.out_keys[0]: node_embedding, self.out_keys[1]: edge_embedding}
+        # prepare layer output
+        out_dict = dict()
+
+        if self.with_node_embedding:
+            out_dict[self.out_keys[0]] = node_embedding
+
+        if self.with_edge_embedding:
+            idx = 1 if self.with_node_embedding else 0
+            out_dict[self.out_keys[idx]] = edge_embedding
+
+        return out_dict
 
     def _compute_embeddings(self, n: torch.Tensor, e: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         """Computes the embeddings of node and edge feature matrices.
@@ -201,19 +222,25 @@ class GNNBlock(ShapeNormalizationBlock):
         for i in range(self.n_layers):
             last_layer = i == self.n_layers - 1
 
-            # embed nodes
-            n = self.node_layers[i](n)
+            if (self.with_node_embedding or self.node2edge_aggr or self.edge2node_aggr) and \
+                    i < len(self.node_layers):
 
-            # embed edges
-            e = self.edge_layers[i](e)
+                # embed nodes
+                n = self.node_layers[i](n)
 
-            # message passing: node -> node
-            if self.node2node_aggr:
-                n = self.n2n_aggregation(n)
+                # message passing: node -> node
+                if self.node2node_aggr:
+                    n = self.n2n_aggregation(n)
 
-            # message passing: edge -> edge
-            if self.edge2edge_aggr:
-                e = self.e2e_aggregation(e)
+            if (self.with_edge_embedding or self.edge2node_aggr or self.node2edge_aggr) and \
+                    i < len(self.edge_layers):
+
+                # embed edges
+                e = self.edge_layers[i](e)
+
+                # message passing: edge -> edge
+                if self.edge2edge_aggr:
+                    e = self.e2e_aggregation(e)
 
             # message passing: edge -> node I
             e2n = None

maze/test/perception/blocks/test_graph_nn.py
@@ -6,10 +6,11 @@ import torch.nn
 
 from maze.perception.blocks.feed_forward.graph_nn import GNNBlock
 from maze.test.perception.perception_test_utils import build_multi_input_dict
+from maze.train.utils.train_utils import compute_gradient_norm
 
 
-def test_GNN_block():
-    """ perception test """
+def run_gnn_config(node2node_aggr: bool, edge2node_aggr: bool, node2edge_aggr: bool, edge2edge_aggr: bool,
+                   with_node_embedding: bool, with_edge_embedding: bool):
 
     n_nodes = 7
     edges = [(0, 1), (0, 2), (1, 3), (4, 5), (2, 5), (0, 3), (0, 6), (1, 5)]
@@ -21,26 +22,79 @@ def test_GNN_block():
                    ([100, n_nodes, n_node_features], [100, n_edges, n_edge_features])]:
         in_dict = build_multi_input_dict(dims=[d0, d1])
 
+        out_keys = []
+        if with_node_embedding:
+            out_keys.append("node_embedding")
+        if with_edge_embedding:
+            out_keys.append("edge_embedding")
+
         net = GNNBlock(in_keys=["in_key_0", "in_key_1"],
-                       out_keys=["node_embedding", "edge_embedding"],
+                       out_keys=out_keys,
                        in_shapes=[(n_nodes, n_node_features), [n_edges, n_edge_features]],
                        edges=edges,
                        aggregate="sum",
-                       hidden_units=[16, 8],
+                       hidden_units=[8, 16, 8],
                        non_lin=torch.nn.ReLU,
                        with_layer_norm=True,
-                       node2node_aggr=True,
-                       edge2node_aggr=True,
-                       node2edge_aggr=True,
-                       edge2edge_aggr=True)
+                       node2node_aggr=node2node_aggr,
+                       edge2node_aggr=edge2node_aggr,
+                       node2edge_aggr=node2edge_aggr,
+                       edge2edge_aggr=edge2edge_aggr,
+                       with_node_embedding=with_node_embedding,
+                       with_edge_embedding=with_edge_embedding)
+
+        net.train()
+        optimizer = torch.optim.Adam(params=net.parameters())
+        optimizer.zero_grad()
+
+        out_dict = net(in_dict)
+
+        loss = 0.0
+        if net.with_node_embedding:
+            loss += out_dict["node_embedding"].mean()
+        if net.with_edge_embedding:
+            loss += out_dict["edge_embedding"].mean()
+
+        # propagate gradients
+        loss.backward()
+        optimizer.step()
+
+        # compute gradient and l2 norm
+        compute_gradient_norm(net.parameters())
+        sum([param.norm() for param in net.parameters()])
 
         str(net)
         out_dict = net(in_dict)
 
         assert isinstance(out_dict, Dict)
-        assert np.all(~np.isnan(out_dict["node_embedding"].detach().cpu().numpy()))
-        assert np.all(~np.isnan(out_dict["edge_embedding"].detach().cpu().numpy()))
+        if net.with_node_embedding:
+            assert np.all(~np.isnan(out_dict["node_embedding"].detach().cpu().numpy()))
+            assert out_dict[net.out_keys[0]].shape[-1] == net.hidden_units[-1]
+        if net.with_edge_embedding:
+            assert np.all(~np.isnan(out_dict["edge_embedding"].detach().cpu().numpy()))
+            idx = 1 if net.with_node_embedding else 0
+            assert out_dict[net.out_keys[idx]].shape[-1] == net.hidden_units[-1]
 
         assert set(net.out_keys).issubset(set(out_dict.keys()))
-        assert out_dict[net.out_keys[0]].shape[-1] == net.hidden_units[-1]
-        assert out_dict[net.out_keys[1]].shape[-1] == net.hidden_units[-1]
+
+
+def test_gnn_block():
+    """ perception test """
+
+    # node and edge embedding
+    run_gnn_config(node2node_aggr=True, edge2node_aggr=True, node2edge_aggr=True, edge2edge_aggr=True,
+                   with_node_embedding=True, with_edge_embedding=True)
+    run_gnn_config(node2node_aggr=True, edge2node_aggr=False, node2edge_aggr=False, edge2edge_aggr=True,
+                   with_node_embedding=True, with_edge_embedding=True)
+
+    # node only embedding
+    run_gnn_config(node2node_aggr=True, edge2node_aggr=False, node2edge_aggr=False, edge2edge_aggr=False,
+                   with_node_embedding=True, with_edge_embedding=False)
+    run_gnn_config(node2node_aggr=True, edge2node_aggr=True, node2edge_aggr=True, edge2edge_aggr=True,
+                   with_node_embedding=True, with_edge_embedding=False)
+
+    # edge only embedding
+    run_gnn_config(node2node_aggr=False, edge2node_aggr=False, node2edge_aggr=False, edge2edge_aggr=True,
+                   with_node_embedding=False, with_edge_embedding=True)
+    run_gnn_config(node2node_aggr=True, edge2node_aggr=True, node2edge_aggr=True, edge2edge_aggr=True,
+                   with_node_embedding=False, with_edge_embedding=True)